6f668d2b8d8df1415f7171fd8f56fdea8ca37229
Ananth Rasu <ananth09121998@gmail.com>
2026-09-20 10:25:02 +0530

    'addLogs'

--- src/main/java/com/example/employeebatch/config/BatchConfig.java
+++ src/main/java/com/example/employeebatch/config/BatchConfig.java
@@ -96,6 +96,7 @@ public class BatchConfig {
         return new EmployeeCsvPartitioner(Path.of(csvFile), gridSize);
     }
 
+//commented this line due to unexpected  runtime error
     @Bean
     public Step employeeWorkerStep(JobRepository jobRepository,
                                    PlatformTransactionManager transactionManager,
--- src/main/java/com/example/employeebatch/exception/CsvDataException.java
+++ src/main/java/com/example/employeebatch/exception/CsvDataException.java
@@ -1,5 +1,6 @@
 package com.example.employeebatch.exception;
 
+//error handling...
 public class CsvDataException extends RuntimeException {
     public CsvDataException(String message) {
         super(message);
